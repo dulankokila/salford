13a129f6e21bd0fcee15419c4f856f41b5f9dcd6
ThimiraAshan <ashanthimira4@gmail.com>
2023-10-05 19:09:04 +0530

    edited

--- homenew.php
+++ homenew.php
@@ -3,7 +3,7 @@ require "connection.php";
 session_start();
 if (isset($_SESSION["u"])) {
 
-    $data = $_SESSION["u"];
+    $email = $_SESSION["u"]["email"];
 
 ?>
     <!DOCTYPE html>
@@ -17,7 +17,7 @@ if (isset($_SESSION["u"])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
 
         <link rel="stylesheet" href="css/style.css">
-        <link rel="icon" href="images/black.png" />
+        <link rel="icon" href="images/black.jpg" />
         <link rel="stylesheet" href="bootstrap.css">
 
     </head>
@@ -33,45 +33,80 @@ if (isset($_SESSION["u"])) {
 
                     <div class="row ms-4 d-flex justify-content-center">
 
-                        <div class="col-5 mx-3 border border-3 b1">
-                            <label class="text-center border-bottom border-3 border-secondary">News</label>
-                            <div class="row m-3 border border-3 news">
-                                <label class="mt-2 fw-bold">
-                                    What is SALFORD ?
+                        <div class="col-5 mx-3 rounded bg-dark ">
+                            <label class="text-center   text-white">News</label>
+                            <div class="row m-3 ">
+                                <label class="mt-2 fw-bold text-white">
+                                    Techies at Meta and Google do "fake work", alleges tech veteran | Tech
                                 </label>
-                                <img src="images/logo.png">
+                                <img src="images/Pichai-says-some-employees-have-to-share-desks-and-offices-details-inside.webp">
                             </div>
                         </div>
-                        <div class="col-5 border border-3 b1">
-                            <label class="text-center border-bottom border-3 border-secondary">Project</label>
-                            <div class="row m-3">
-                                <div class="col-12">
-                                    <button class="button">Create projects</button>
-                                </div>
-                                <div class=" row col-12 mt-5">
+                        <div class="col-5 border border-3 rounded b1">
+                            <div class="conatiner">
+
+                                <label class="text-center  rounded">Project</label>
 
 
-                                    <div class="col-5">
-                                        <h3>my projects</h3>
-                                    </div>
-                                    <div class="col-5 justify-content-center">
-                                        <input type="text" class="form-control" placeholder="Searsh your projects">
-                                    </div>
+
+                                <div class="row m-3">
                                     <div class="col-12">
-                                        <!-- search projects -->
+                                        <?php
+                                        $details_rs = Database::search("SELECT * FROM `user`  WHERE `email`='" . $email . "'");
+                                        $image_rs = Database::search("SELECT * FROM `image` WHERE `user_email`='" . $email . "'");
+                                        $image_data = $image_rs->fetch_assoc();
+                                        $data = $details_rs->fetch_assoc();
+                                        ?>
+                                        <img src="<?php echo $image_data["path"]; ?>" class="rounded mt-5" style="width: 150px;" />
+                                        <br /> Logged As <span class="text-primary"><?php echo $data["name"]; ?></span>
+                                        <br />
+                                        <?php echo $data["email"]; ?>
                                     </div>
-
-                                </div>
-                                <div class=" row col-12 mt-5">
+                                    <div class=" row col-12 mt-5">
 
 
-                                    <div class="col-5">
-                                        <h6 class="fw-bold">members projects</h6>
+                                        <div class="col-5">
+                                            <h3>my projects</h3>
+                                        </div>
+                                        <div class="col-5 justify-content-center">
+                                            <input type="text" class="form-control" placeholder="Searsh your projects">
+                                        </div>
+                                        <div class="col-12">
+                                            <!-- search projects -->
+                                        </div>
+
+                                    </div>
+                                    <div class=" row col-12 mt-5">
+
+
+                                        <div class="col-5">
+                                            <h6 class="fw-bold">members projects</h6>
+                                            <div class="col-12">
+                                                <?php
+
+
+                                                $pro_rs =       Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON projects.id=user_has_projects.projects_id INNER JOIN `user_role` ON user_role.id=user_has_projects.user_role_id WHERE `role`='member'");
+                                                $pro_num =  $pro_rs->num_rows;
+                                                $user_data = $pro_rs->fetch_assoc();
+
+                                                for ($x = 0; $x < $pro_num; $x++) {
+                                                ?>
+                                                    <a href=""><?php echo ($user_data["name"]); ?></a> <br>
+                                                <?php
+                                                }
+
+
+                                                ?>
+                                            </div>
+                                        </div>
+                                        <div class="col-5 justify-content-center">
+                                            <h6 class="fw-bold">Admin projects</h6>
+                                        </div>
                                         <div class="col-12">
                                             <?php
 
 
-                                            $pro_rs =       Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON projects.id=user_has_projects.projects_id INNER JOIN `user_role` ON user_role.id=user_has_projects.user_role_id WHERE `role`='member'");
+                                            $pro_rs =       Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON projects.id=user_has_projects.projects_id INNER JOIN `user_role` ON user_role.id=user_has_projects.user_role_id WHERE `role`='Admin'");
                                             $pro_num =  $pro_rs->num_rows;
                                             $user_data = $pro_rs->fetch_assoc();
 
@@ -84,38 +119,51 @@ if (isset($_SESSION["u"])) {
 
                                             ?>
                                         </div>
+
                                     </div>
-                                    <div class="col-5 justify-content-center">
-                                        <h6 class="fw-bold">Admin projects</h6>
-                                    </div>
-                                    <div class="col-12">
-                                        <?php
 
-
-                                        $pro_rs =       Database::search("SELECT * FROM `projects` INNER JOIN `user_has_projects` ON projects.id=user_has_projects.projects_id INNER JOIN `user_role` ON user_role.id=user_has_projects.user_role_id WHERE `role`='Admin'");
-                                        $pro_num =  $pro_rs->num_rows;
-                                        $user_data = $pro_rs->fetch_assoc();
-
-                                        for ($x = 0; $x < $pro_num; $x++) {
-                                        ?>
-                                            <a href=""><?php echo ($user_data["name"]); ?></a> <br>
-                                        <?php
-                                        }
-
-
-                                        ?>
-                                    </div>
 
                                 </div>
-
-
-
                             </div>
+
+                        </div>
+
+                    </div>
+                    <div class="container ">
+                        <div class="row mx-3">
+                            <div class="card mx-3" style="width: 18rem;">
+                                <img src="images/1649106561425.webp" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    
+                                    <p class="card-text">Elon Exposes twitter as X.</p>
+                                    <a href="#" class="btn btn-secondary">Find Out</a>
+                                </div>
+                            </div>
+                       
+                      
+                            <div class="card mx-3" style="width: 18rem;">
+                                <img src="images/5247b370-03c7-11ee-bff3-6eb6001555e5.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    
+                                    <p class="card-text">Find Out ios 17 new Features.</p>
+                                    <a href="#" class="btn btn-secondary">Find Out</a>
+                                </div>
+                            </div>
+                       
+
+                            <div class="card mx-3" style="width: 18rem;">
+                                <img src="images/media_1c9568bea14470395f2c881516fe1698dd9d1de47.jpeg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                
+                                    <p class="card-text">Adobe Creative clouds are very Optimized now.</p>
+                                    <a href="#" class="btn btn-secondary">Find Out</a>
+                                </div>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
