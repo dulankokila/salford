22181ecd2adb1a15b0e555ea56a0655f78141bec
ThimiraAshan <ashanthimira4@gmail.com>
2023-10-05 16:02:52 +0530

    session

--- userProfile.php
+++ userProfile.php
@@ -24,8 +24,33 @@
 
             <?php include "header.php"; ?>
 
-            <br/>
-            <br/>
+            <br />
+            <br />
+
+            <?php
+
+            require "connection.php";
+
+            if (isset($_SESSION["u"])) {
+
+                $email = $_SESSION["u"]["email"];
+
+
+                $details_rs = Database::search("SELECT * FROM `user`  WHERE `email`='" . $email . "'");
+
+                $image_rs = Database::search("SELECT * FROM `image` WHERE `user_email`='" . $email . "'");
+
+
+                $data = $details_rs->fetch_assoc();
+                $image_data = $image_rs->fetch_assoc();
+               
+
+
+            ?>
+
+
+
+
 
 
                 <div class="col-12" style="background-color:#183D3D;">
@@ -36,14 +61,14 @@
 
                                 <div class="col-md-3 border-end">
                                     <div class="d-flex flex-column align-items-center text-center p-3 py-5">
- 
+
 
 
                                         <span class="fw-bold"> </span>
                                         <span class="fw-bold text-black-50"></span>
 
-                                        <input type="file" class="d-none"/><img src="profile_img/profile.png" style="height: 200px;" />
-                                        <label for="profileimg" class="btn btn-primary mt-5" >Update Profile Image</label>
+                                        <input type="file" class="d-none" /><img src="profile_img/profile.png" style="height: 200px;" />
+                                        <label for="profileimg" class="btn btn-primary mt-5">Update Profile Image</label>
 
                                     </div>
                                 </div>
@@ -58,25 +83,19 @@
                                         <div class="row mt-4">
 
                                             <div class="col-6">
-                                                <label class="form-label">First Name</label>
-                                                <input type="text" class="form-control" style="border-radius: 20px;"/>
+                                                <label class="form-label">Full Name</label>
+                                                <input type="text" class="form-control" style="border-radius: 20px;" value="<?php echo $data["name"]; ?>"  />
                                             </div>
 
-                                            <div class="col-6">
-                                                <label class="form-label">Last Name</label>
-                                                <input type="text" class="form-control" style="border-radius: 20px;"/>
-                                            </div>
+                                           
 
-                                            <div class="col-12">
-                                                <label class="form-label">Mobile</label>
-                                                <input type="text" class="form-control" style="border-radius: 20px;"/>
-                                            </div>
+                                           
 
                                             <div class="col-12">
-                                                <label class="form-label" >Password</label>
-                                                <div class="input-group"  >
-                                                    <input type="password" class="form-control" style="border-top-left-radius: 20px; border-bottom-left-radius: 20px;"/>
-                                                    
+                                                <label class="form-label">Password</label>
+                                                <div class="input-group">
+                                                    <input type="password" class="form-control" style="border-top-left-radius: 20px; border-bottom-left-radius: 20px;" value="<?php echo $data["name"]; ?>"/>
+
                                                     <span class="input-group-text bg-primary" style="border-bottom-right-radius: 20px;  border-top-right-radius: 20px">
                                                         <i class="bi bi-eye-slash-fill text-white"></i>
                                                     </span>
@@ -85,14 +104,14 @@
 
                                             <div class="col-12">
                                                 <label class="form-label">Email</label>
-                                                <input type="text" class="form-control" style="border-radius: 20px;" readonly/>
+                                                <input type="text" class="form-control" style="border-radius: 20px;"value="<?php echo $data["email"]; ?>" />
                                             </div>
 
                                             <div class="col-12">
                                                 <label class="form-label">Registered Date</label>
-                                                <input type="text" class="form-control" style="border-radius: 20px;" readonly/>
+                                                <input type="text" class="form-control" style="border-radius: 20px;" readonly />
                                             </div>
-   
+
 
                                             <div class="col-12 d-grid mt-3">
                                                 <button class="btn btn-primary">Update My Profile</button>
@@ -103,14 +122,22 @@
                                     </div>
                                 </div>
 
-                               
+
                             </div>
                         </div>
 
                     </div>
                 </div>
         </div>
+    <?php
+
+            } else {
+                header("Location:http://localhost/eshop_java/home.php");
+            }
+
+    ?>
     </div>
+
 
     <script src="bootstrap.bundle.js"></script>
     <script src="script.js"></script>
